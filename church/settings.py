7b49ca22fe7a421d72721ec4d77474b2972b4360
vishnuprasad404 <vishnuprasadn2000@gmail.com>
2022-07-15 19:20:48 -0400

    landing page updated

--- church/settings.py
+++ church/settings.py
@@ -127,8 +127,11 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/uploads')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
