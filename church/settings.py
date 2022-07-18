770710024e02a8ffd0727ecb805ce27fb646d197
vishnuprasad404 <vishnuprasadn2000@gmail.com>
2022-07-18 16:02:35 -0400

    settings.py updated

--- church/settings.py
+++ church/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e%9$_!-e)h!rx%q7&h%f^tz94j6df6t619zat_79(+2^cu@8)h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
